bddfb5bbccc65257918d07959f652d0bd82b5855
Baxtiberliev <86955701+Baxtiberliev@users.noreply.github.com>
2021-10-09 00:12:14 +0500

    Add files via upload

---
+++ BSR-MENU/BSR-PROYEKT/Number/script.js
@@ -0,0 +1,604 @@
+const divB = document.querySelector(".div1")
+const divE = document.querySelector(".div2")
+const divU = document.querySelector(".div3")
+const divT = document.querySelector(".div4")
+const divV = document.querySelector(".div5")
+const divA = document.querySelector(".div6")
+const divJ = document.querySelector(".div7")
+const divS = document.querySelector(".div8")
+const divTO = document.querySelector(".div9")
+const divON = document.querySelector(".div10")
+const divOB = document.querySelector(".div11")
+const divOE = document.querySelector(".div12")
+const divOU = document.querySelector(".div13")
+const divOT = document.querySelector(".div14")
+const divOV = document.querySelector(".div15")
+const btn = document.getElementById("btn")
+function btnB(){
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divV.className = "divV"
+    }, 600);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 800);
+    setTimeout(function() {
+        divOB.className = "divOB"
+    }, 1000);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 1200);
+    
+    
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 2000);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 2200);
+    setTimeout(function() {
+        divV.className = "div5"
+    }, 2400);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 2600);
+    setTimeout(function() {
+        divOB.className = "div11"
+    }, 2800);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 3000);
+}
+function btnE(){
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 600);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 800);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1000);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 1200);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 1400);
+    setTimeout(function() {
+        divON.className = "divON"
+    }, 1600);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 1800);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 2000);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 2200);
+    
+    
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 3000);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 3200);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 3600);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 3800);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 4000);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 4200);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 4400);
+    setTimeout(function() {
+        divON.className = "div10"
+    }, 4600);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 4800);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 5000);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 5200);
+}
+function btnU(){
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 600);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 800);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1000);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 1200);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 1400);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1200);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1400);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 1600);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 1800);
+    
+    
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 2600);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 2800);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 3000);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 3200);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 3400);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 3600);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 3800);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 4000);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 4200);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 4600);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 4800);
+}
+function btnT(){
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 200);
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 400);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 600);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 800);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1000);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 1200);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 1400);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1200);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1400);
+    
+    
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 2200);
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 2400);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 2600);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 2800);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 3000);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 3200);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 3400);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 3200);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 3400);
+}
+function btnV(){
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 600);
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 800);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 1000);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 1200);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1400);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1600);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1800);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 2000);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 2200);
+    
+    
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 3000);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 3200);
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 3400);
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 3600);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 3800);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 4000);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 4200);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 4400);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 4600);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 4800);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 5000);
+}
+function btnA(){
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 600);
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 800);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 1000);
+    setTimeout(function() {
+        divON.className = "divON"
+    }, 1200);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 1400);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 1600);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1800);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 2000);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 2200);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 2400);
+    
+    
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 3200);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 3400);
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 3600);
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 3800);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 4000);
+    setTimeout(function() {
+        divON.className = "div10"
+    }, 4200);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 4400);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 4600);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 4800);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 5000);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 5200);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 5400);
+}
+function btnJ(){
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 600);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 800);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1000);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1200);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1400);
+    
+    
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 2200);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 2400);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 2600);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 2800);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 3000);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 3200);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 3400);
+}
+function btnS(){
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 200);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 400);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 600);
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 400);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 600);
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 800);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 800);
+    setTimeout(function() {
+        divON.className = "divON"
+    }, 800);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1000);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 1000);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1200);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 1200);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 1400);
+    
+    
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 2200);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 2400);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 2600);
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 2400);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 2600);
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 2800);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 2800);
+    setTimeout(function() {
+        divON.className = "div10"
+    }, 2800);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 3000);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 3000);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 3200);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 3200);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 3400);
+}
+function btnTO(){
+    setTimeout(function() {
+        divS.className = "divS"
+    }, 200);
+    setTimeout(function() {
+        divJ.className = "divJ"
+    }, 400);
+    setTimeout(function() {
+        divT.className = "divT"
+    }, 600);
+    setTimeout(function() {
+        divB.className = "divB"
+    }, 800);
+    setTimeout(function() {
+        divE.className = "divE"
+    }, 1000);
+    setTimeout(function() {
+        divU.className = "divU"
+    }, 1200);
+    setTimeout(function() {
+        divA.className = "divA"
+    }, 1400);
+    setTimeout(function() {
+        divTO.className = "divTO"
+    }, 1600);
+    setTimeout(function() {
+        divOE.className = "divOE"
+    }, 1800);
+    setTimeout(function() {
+        divOV.className = "divOV"
+    }, 2000);
+    setTimeout(function() {
+        divOT.className = "divOT"
+    }, 2200);
+    setTimeout(function() {
+        divOU.className = "divOU"
+    }, 2400);
+    
+    
+    setTimeout(function() {
+        divS.className = "div8"
+    }, 3200);
+    setTimeout(function() {
+        divJ.className = "div7"
+    }, 3400);
+    setTimeout(function() {
+        divT.className = "div4"
+    }, 3600);
+    setTimeout(function() {
+        divB.className = "div1"
+    }, 3800);
+    setTimeout(function() {
+        divE.className = "div2"
+    }, 4000);
+    setTimeout(function() {
+        divU.className = "div3"
+    }, 4200);
+    setTimeout(function() {
+        divA.className = "div6"
+    }, 4400);
+    setTimeout(function() {
+        divTO.className = "div9"
+    }, 4600);
+    setTimeout(function() {
+        divOE.className = "div12"
+    }, 4800);
+    setTimeout(function() {
+        divOV.className = "div15"
+    }, 5000);
+    setTimeout(function() {
+        divOT.className = "div14"
+    }, 5200);
+    setTimeout(function() {
+        divOU.className = "div13"
+    }, 5400);
+}
