28a37639db2726d62ef597b0664f190883d72f29
Baxtiberliev <86955701+Baxtiberliev@users.noreply.github.com>
2021-10-09 00:14:14 +0500

    Add files via upload

---
+++ BSR-MENU/BSR-PROYEKT/Х|О Game/script.js
@@ -0,0 +1,355 @@
+const bir = document.getElementById('x01')
+const eki = document.getElementById('x02')
+const ush = document.getElementById('x03')
+const tort = document.getElementById('x04')
+const bes = document.getElementById('x05')
+const alti = document.getElementById('x06')
+const jeti = document.getElementById('x07')
+const segiz = document.getElementById('x08')
+const togiz = document.getElementById('x09')
+
+
+
+const hx = document.getElementById('X')
+const ho = document.getElementById('O')
+
+
+function xx() {
+    hx.className = "x"
+}
+function oo() {
+    ho.className = "o"
+}
+
+
+function birr() {
+    if(hx.className === "x") {
+        bir.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        bir.className = "ykis"
+    }else if(ho.className === "o") {
+        bir.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        bir.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            bir.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            bir.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            bir.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            bir.className = "nool"
+        }else{
+            bir.innerHTML = ""
+        }
+    }
+}
+function ekii() {
+    if(hx.className === "x") {
+        eki.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        eki.className = "ykis"
+    }else if(ho.className === "o") {
+        eki.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        eki.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            eki.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            eki.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            eki.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            eki.className = "nool"
+        }else{
+            eki.innerHTML = ""
+        }
+    }
+}
+function ushh() {
+    if(hx.className === "x") {
+        ush.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        ush.className = "ykis"
+    }else if(ho.className === "o") {
+        ush.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        ush.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            ush.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            ush.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            ush.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            ush.className = "nool"
+        }else{
+            ush.innerHTML = ""
+        }
+    }
+}
+function tortt() {
+    if(hx.className === "x") {
+        tort.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        tort.className = "ykis"
+    }else if(ho.className === "o") {
+        tort.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        tort.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            tort.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            tort.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            tort.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            tort.className = "nool"
+        }else{
+            tort.innerHTML = ""
+        }
+    }
+}
+function bess() {
+    if(hx.className === "x") {
+        bes.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        bes.className = "ykis"
+    }else if(ho.className === "o") {
+        bes.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        bes.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            bes.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            bes.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            bes.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            bes.className = "nool"
+        }else{
+            bes.innerHTML = ""
+        }
+    }
+}
+function altii() {
+    if(hx.className === "x") {
+        alti.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        alti.className = "ykis"
+    }else if(ho.className === "o") {
+        alti.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        alti.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            alti.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            alti.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            alti.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            alti.className = "nool"
+        }else{
+            alti.innerHTML = ""
+        }
+    }
+}
+function jetii() {
+    if(hx.className === "x") {
+        jeti.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        jeti.className = "ykis"
+    }else if(ho.className === "o") {
+        jeti.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        jeti.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            jeti.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            jeti.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            jeti.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            jeti.className = "nool"
+        }else{
+            jeti.innerHTML = ""
+        }
+    }
+}
+function segizz() {
+    if(hx.className === "x") {
+        segiz.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        segiz.className = "ykis"
+    }else if(ho.className === "o") {
+        segiz.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        segiz.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            segiz.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            segiz.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            segiz.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            segiz.className = "nool"
+        }else{
+            segiz.innerHTML = ""
+        }
+    }
+}
+function togizz() {
+    if(hx.className === "x") {
+        togiz.innerHTML = "x"
+        hx.className = "xx"
+        ho.className = "ooo"
+        togiz.className = "ykis"
+    }else if(ho.className === "o") {
+        togiz.innerHTML = "o"
+        hx.className = "xxx"
+        ho.className = "oo"
+        togiz.className = "nool"
+    }else{
+        if(hx.className === "xxx") {
+            togiz.innerHTML = "x"
+            ho.className = "o"
+            hx.className = "xx"
+            togiz.className = "ykis"
+        }else if(ho.className ==="ooo") {
+            togiz.innerHTML = "o"
+            hx.className = "x"
+            ho.className = "oo"
+            togiz.className = "nool"
+        }else{
+            togiz.innerHTML = ""
+        }
+    }
+}
+
+
+const reset = document.getElementById('clear')
+reset.onclick = function() {
+    hx.className = "";
+    ho.className = "";
+    
+    
+    bir.innerHTML = "";
+    bir.className = "";
+    
+    eki.innerHTML = "";
+    eki.className = "";
+    
+    ush.innerHTML = "";
+    ush.className = "";
+    
+    tort.innerHTML = "";
+    tort.className = "";
+    
+    bes.innerHTML = "";
+    bes.className = "";
+    
+    alti.innerHTML = "";
+    alti.className = "";
+    
+    jeti.innerHTML = "";
+    jeti.className = "";
+    
+    segiz.innerHTML = "";
+    segiz.className = "";
+    
+    togiz.innerHTML = "";
+    togiz.className = "";
+}
+
+
+document.addEventListener("click", 
+function sapar() {
+    if((bir.className === "ykis") && (eki.className === "ykis") && (ush.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((bir.className === "nool") && (eki.className === "nool") && (ush.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((bir.className === "ykis") && (tort.className === "ykis") && (jeti.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((bir.className === "nool") && (tort.className === "nool") && (jeti.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((jeti.className === "ykis") && (segiz.className === "ykis") && (togiz.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((jeti.className === "nool") && (segiz.className === "nool") && (togiz.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((togiz.className === "ykis") && (alti.className === "ykis") && (ush.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((togiz.className === "nool") && (alti.className === "nool") && (ush.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((bir.className === "ykis") && (bes.className === "ykis") && (togiz.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((bir.className === "nool") && (bes.className === "nool") && (togiz.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((ush.className === "ykis") && (bes.className === "ykis") && (jeti.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((ush.className === "nool") && (bes.className === "nool") && (jeti.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((eki.className === "ykis") && (bes.className === "ykis") && (segiz.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((eki.className === "nool") && (bes.className === "nool") && (segiz.className === "nool")) {
+        alert("O победиль")
+    }
+    else if((tort.className === "ykis") && (bes.className === "ykis") && (alti.className === "ykis")) {
+        alert("X победиль")
+    }
+    else if((tort.className === "nool") && (bes.className === "nool") && (alti.className === "nool")) {
+        alert("O победиль")
+    }
+})
